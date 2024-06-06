6f4f220bd318a9ae4bada0f797fb3f06782ee8e4
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2024-06-06 18:21:57 +0530

    connect to  mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import mongoose, { connect } from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path:'./env'});
+import dotenv from 'dotenv';
+import connectDB from "./db/indexDB.js";
 
+dotenv.config({path: './env'});
 
+connectDB();
+
+/*
+import express from 'express'
+const app = express();
 ;( async ()=>{
     try{
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+       app.on('error', () => {
+        console.log('ERROR', error);
+        throw error;
+       });
+
+       app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+       });
     }catch(error){
         console.log('ERROR: ', error);
+        throw error;
     }
 })();
 
@@ -14,3 +31,4 @@ function connectDB(){
     
 };
 
+*/
